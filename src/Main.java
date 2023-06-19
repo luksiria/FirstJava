9eb2884d80fbaa58e040f1cb641b0b67336b52f1
Kris <kristina_lacrima@mail.ru>
2023-06-19 17:08:54 +0300

    delete}

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
@@ -16,8 +14,5 @@ public class Main {
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ки/ка) специй");
 
-    }
-}
-        }
     }
 }
